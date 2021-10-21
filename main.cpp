2b131696c1cfa8bab167ab9a873dfd79fa474454
Shaunak Kale <epicshaunak@gmail.com>
2021-10-21 00:57:42 -0700

    hi

--- main.cpp
+++ main.cpp
@@ -6,12 +6,12 @@ int main(int argc, char *argv[])
 {
     ifstream hashArgs(argv[1]);
     Dictionary table(argv[2], argv[1]);
-    cout
-        << "Queries:" << endl;
-    ifstream queries(argv[3]);
-    string query;
-    while (getline(queries, query))
-    {
-        table.find(query);
-    }
+    // cout
+    //     << "Queries:" << endl;
+    // ifstream queries(argv[3]);
+    // string query;
+    // while (getline(queries, query))
+    // {
+    //     table.find(query);
+    // }
 }
