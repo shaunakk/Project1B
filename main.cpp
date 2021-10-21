9d9fad92a22096fd083a36b511082de07f16bf48
Shaunak Kale <epicshaunak@gmail.com>
2021-10-21 00:50:37 -0700

    hi

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     ifstream hashArgs(argv[1]);
-    Dictionary table(argv[1], argv[2]);
+    Dictionary table(argv[2], argv[1]);
     cout
         << "Queries:" << endl;
     ifstream queries(argv[3]);
